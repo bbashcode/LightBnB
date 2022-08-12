995ba417c20e22410a7922a69d33e32aea30fc43
bbashcode <binbash.code@gmail.com>
2022-08-12 10:53:47 +0000

    update getUserWithId method to properly query the db

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -37,7 +37,16 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool
+  .query(`SELECT * FROM users WHERE users.id = $1`, [id])
+  .then((result) => {
+    if(result.rows){
+      return result.rows[0];
+    } else {
+      return null;
+    }
+  })
+  .catch((err) => err.message);
 }
 exports.getUserWithId = getUserWithId;
 
